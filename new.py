d9aa1c7f893b1775be73c9cdd5e53f762963f577
Mansi Singh <72257441+mansi1811-s@users.noreply.github.com>
2023-11-09 00:30:35 +0530

    Update new.py

--- new.py
+++ new.py
@@ -198,6 +198,7 @@ def get_objects():
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port="5000",debug=True))# pylint: disable=superfluous-parens
 
+# 
 
 # if __name__ == "__main__":
 #    app.run(debug=True)
